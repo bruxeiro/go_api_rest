ce74d9bb28646996ccf9737f41e1fa642dcd2694
Bruxeiro <gabrielrosabueno@hotmail.com>
2023-04-10 20:00:04 -0300

    Adicionadas funções para adicionar, deletar e edit

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -31,3 +31,29 @@ func RetornaPerso(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&p, id)
 	json.NewEncoder(w).Encode(p)
 }
+
+func CriaPerso(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+	database.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func DeletaPerso(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func EditaPerso(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+
+	database.DB.First(&personalidade, id)
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.DB.Save(&personalidade)
+	json.NewEncoder(w).Encode(personalidade)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -14,8 +14,11 @@ func HandleResquest() {
 	//Criando router para o gorilla mux
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.CriaPerso).Methods("Post")
 	r.HandleFunc("/api/personalidades", controllers.TodasPerso).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPerso).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPerso).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPerso).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
