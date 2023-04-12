b94993605f3232420d2678ad189b7c0e76bdba8c
Bruxeiro <gabrielrosabueno@hotmail.com>
2023-04-12 20:18:49 -0300

    Adicionando frontend e conectando com o back

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 	"go_api_rest/controllers"
 	"go_api_rest/middleware"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -21,6 +22,8 @@ func HandleResquest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPerso).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPerso).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPerso).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+
+	//Conectando a API Rest com o frontend baseado em react
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 
 }
